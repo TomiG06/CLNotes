28bce6fd5d3745e17a94be91d49a30736f5e2912
TomiG06 <thomasgkountas@gmail.com>
2022-03-09 08:44:11 +0200

    removed success message small changes on error messages exit when illegal number is met

--- src/main.c
+++ src/main.c
@@ -3,16 +3,16 @@
 int main(int argc, char* argv[]) {
     for(size_t x = 1; x<argc; ++x) argv[x] = argtolower(argv[x]);
     if(!strcmp(argv[1], "-c")) {
-        if(argc == 2) {
-            fprintf(stderr, "Second argument must be the note you want to add\n");
+        if(argc != 3) {
+            fprintf(stderr, "Must receive 3 arguments\nReceived %d\n", argc);
             return 1;
         }
         char as = addNote(argv[2]);
-        if(as == 1) printf("Successfully added!\n");
+        if(as == 1) return 0;
         else {
             if(!as) fprintf(stderr, "Note lengthier than allowed\nLength: %ld\nMaximum: 100\n", strlen(argv[2]));
             else if(as == -2) fprintf(stderr, "Note can't contain delimeter '~'\n");
-            else if(as == 2) fprintf(stderr, "Instance already in database\n");
+            else if(as == 2) fprintf(stderr, "Instance already exists\n");
             else fprintf(stderr, "You have reached the maximum amount of notes allowed\n");
             return 1;
         }
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
         else if(!strcmp(argv[2], "-v")) displayNotes(1);
         else if(!strcmp(argv[2], "-x")) displayNotes(0);
         else {
-            fprintf(stderr, "Uknown status\n");
+            fprintf(stderr, "Uknown status '%s'\n", argv[2]);
             return 1;
         }
     } else if(!strcmp(argv[1], "-u")) {
@@ -29,15 +29,11 @@ int main(int argc, char* argv[]) {
             fprintf(stderr, "No input note\n");
             return 1;
         }
-        uint8_t ln = lines();
         for(uint8_t x = 2; x<argc; ++x) {
-            if(!argisdigit(argv[x])) {
-                fprintf(stderr, "Expected number, got '%s'\n", argv[x]);
-                continue;
+            if(!argisdigit(argv[x]) || !updateNote(atoi(argv[x]))) {
+                fprintf(stderr, "Illegal number '%s'\n", argv[x]);
+                return 1;
             }
-            uint8_t us = updateNote(atoi(argv[x]), ln);
-            if(us) printf("Note %s updated successfully\n", argv[x]);
-            else fprintf(stderr, "Number %s out of range\nRange: 0 ... %d\n", argv[2], ln);
         }
     } else if(!strcmp(argv[1], "-d")) {
         if(argc == 2) {
@@ -57,13 +53,10 @@ int main(int argc, char* argv[]) {
             return 0;
         }
         for(size_t x = 2; x<argc; ++x) {
-            if(!argisdigit(argv[x])) {
-                fprintf(stderr, "Expected number, got '%s'\n", argv[x]);
-                continue;
+            if(!argisdigit(argv[x]) || !deleteNote(atoi(argv[2])-1)) {
+                fprintf(stderr, "Illegal number '%s'\n", argv[x]);
+                return 1;
             }
-            char ds = deleteNote(atoi(argv[2])-1);
-            if(ds) printf("Note %s deleted successfully\n", argv[x]);
-            else fprintf(stderr, "Number %s out of range\nRange: 0 ... %d\n", argv[2], lines());
         }
     } else if(!strcmp(argv[1], "-h")) {
         help();
